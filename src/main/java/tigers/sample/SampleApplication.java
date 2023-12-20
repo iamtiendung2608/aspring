c26f68aaae408e2c7660b0c98e680a4e60cfad18
Vinh Truong <tpvinhcse@gmail.com>
2023-12-20 11:33:58 +0700

    Update SampleApplication.java

--- src/main/java/tigers/sample/SampleApplication.java
+++ src/main/java/tigers/sample/SampleApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SampleApplication {
@@ -13,7 +14,8 @@ public class SampleApplication {
 	}
 }
 
-@RestController("/hello")
+@RestController
+@RequestMapping("/hello")
 class HelloController {
 	@GetMapping
 	public String hello() {
